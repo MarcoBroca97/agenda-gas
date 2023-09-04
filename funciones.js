0cb9c2e9c52ae10a8d7c89918cb96855f8283de8
MarcoBroca <marco.broca@Weatherford.com>
2023-09-03 21:31:18 -0600

    Video9 curso-gas-avanzado

--- funciones.js
+++ funciones.js
@@ -7,3 +7,12 @@ function obtenerDatosHTML(nombre)
 {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+
+function obtenerContactos()
+{
+    let hoja = SpreadsheetApp.openById("1284YhLRgpS3Cvovn88zum4_t5C47LevWiep6vR3RpcU").getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
+    
+}
